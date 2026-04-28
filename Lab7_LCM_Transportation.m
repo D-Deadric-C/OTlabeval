6924f93580cbf9b138fe8f6372b67ff9d8841997
Suryansh <ssuryansh_be23@thapar.edu>
2026-04-28 10:42:22 +0530

    Update Lab7_LCM_Transportation.m

--- Lab7_LCM_Transportation.m
+++ Lab7_LCM_Transportation.m
@@ -1,76 +1,44 @@
-%% clearning
-clc;
+clc ;
 clear all;
-
-%% The ques 
-% +--------------+-----+-----+-----+-----+------------+
-% |              | D1  | D2  | D3  | D4  | Supply ( ) |
-% +--------------+-----+-----+-----+-----+------------+
-% | S1           | 11  | 13  | 17  | 14  | 10         |
-% | S2           | 16  | 18  | 14  | 10  | 5          |
-% | S3           | 21  | 24  | 13  | 10  | 9          |
-% | S4 (Dummy)   | 0   | 0   | 0   | 0   | 10         |  <-  added to
-% +--------------+-----+-----+-----+-----+------------+     balance out
-% | Demand ->    | 8   | 7   | 15  | 4   | 34 | 34    | <- Balanced after
-% +--------------+-----+-----+-----+-----+------------+     adding dummy
-
-%% Phase 1 
-
-cost = [11 13 17 14;
-     16 18 14 10;
-     21 24 13 10];
-
-demand = [8 7 15 4];
-supply = [10 5 9];
-% supply = [10; 5; 9; 10];
-
-m = size(cost, 1); % number of sources
-n = size(cost, 2); % number of destinations
-
-%% Phase 2
-% Checking balance of the transportation problem
-
-S = sum(supply);
-D = sum(demand); 
-if (S == D)
-    disp("Balanced Transportation Problem")
-elseif S < D
-    disp('Supply is less than demand. Adjustisting....');
-    cost(end + 1, :) = zeros(1, n);
-    supply(end + 1) = D - S;
-else % S > D
-    cost(:, end + 1) = zeros(m, 1);
-    demand(end + 1) = S - D;
+Cost=[11 13 17 14; 16 18 14 10;21 24 13 10];
+supply=[10 5 9 ];
+demand=[8 7 15 4];
+m=size(Cost,1);
+n=size(Cost,2);
+S=sum(supply);
+D=sum(demand);
+if(S==D)
+    disp('BalancedTP')
+elseif(D>S)
+    Cost(end+1,:)=zeros(1,n); % Cost(m+1,:)
+    supply(end+1)=D-S;
+else
+    Cost(:,end+1)=zeros(m,1);
+    demand(end+1)=S-D
 end
-disp('Demand is less than supply. Adjustisting....');
-disp("Balanced TP Table: ")
-disp([cost supply'; demand sum(supply)])
-
-% Updating number of sources and destinations
-[m, n] = size(cost);
-
-%% Phase 3
-% Initializing solution set
-
-X = zeros(m , n);  % Number of allocation variables = m * n
-Icost = cost;
-
-% stopping condition, till all the supplies and demands are 0
-while(any(supply ~= 0) || any(demand ~= 0))
-    min_cost = min(cost(:));
-    [row, col] = find(cost == min_cost); % i,j for cells with min cost
-    y = min(supply(row), demand(col)); % value of the allocation
-    [aloc, index] = max(y);
-    rr = row(index);
-    cc = col(index);
-    X(rr, cc) = aloc; % assiging the value to the allocation
-
+disp("Balanced TP Table ")
+[Cost supply'; demand sum(supply)]
+[m,n]=size(Cost);
+X=zeros(m,n);
+ICost=Cost;
+while(any(supply~=0) || any(demand~=0))
+    min_cost=min(Cost(:))
+    [r c]=find(Cost==min_cost)
+    y=min(supply(r),demand(c))
+    [aloc,index]=max(y)
+    rr=r(index)
+    cc=c(index)
+    X(rr, cc)=aloc;
     supply(rr) = supply(rr) - aloc;
     demand(cc) = demand(cc) - aloc;
-
-    cost(rr, cc) = inf;
+    Cost(rr, cc) = inf; 
 end
-
-cost_eachcell = X.*Icost;
-total_cost = sum(cost_eachcell(:))
+% Calculate total cost of the transportation plan
+Cost_eachcell= X .* ICost;
+Total_Cost=sum(Cost_eachcell(:))
 disp(X)
+
+
+
+
+
